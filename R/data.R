5ed573c02130b6825b37d896b15be5dec6c7440d
Jack M Wolf <38739875+jackmwolf@users.noreply.github.com>
2021-02-09 21:23:11 -0600

    Add new example data with both continuous and binary phenotypes and more predictors

--- R/data.R
+++ R/data.R
@@ -23,3 +23,18 @@
 #'   \item{y1,y2,y3,y4,y5}{5 continuous phenotypes}
 #' }
 "cont_data"
+
+
+#' Simulated example data
+#'
+#' A dataset containing simulated genetic data with 3 SNPs, 3 continuous 
+#' covariates, and 6 continuous phenotypes.
+#'
+#' @format A data frame with 1000 rows and 12 columns:
+#' \describe{
+#'   \item{g1,g2,g3}{Minor allele counts at three sites}
+#'   \item{x1,x2,x3}{Continuous covariates}
+#'   \item{y1,y2,y3}{Continuous phenotypes}
+#'   \item{y4,y5,y6}{Binary phenotypes}
+#' }
+"pcsstools_example"
--- data-raw/make_example_data.R
+++ data-raw/make_example_data.R
@@ -40,3 +40,42 @@ save(bin_data, file = "data/bin_data.RData")
 
 test_data_bin <- make_data(response = "binary", n = 10000, m = 3)
 save(test_data_bin, file = "data/test_data_bin.RData")
+
+
+
+
+
+
+pcsstools_data <- function(n = 1000) {
+  Rho_X <- matrix(c(1, 0.5, 0.1, 0.5, 1, 0.25, 0.1,0.25,  1), nrow = 3)
+  X <- MASS::mvrnorm(n = n, mu = rep(0, 3), Sigma = Rho_X)
+  colnames(X) <- paste0("x", 1:ncol(X))
+  
+  mafs <- c(0.25, 0.4, 0.15)
+  G <- sapply(mafs, function(x) rbinom(n = n, size = 2, prob = x))
+  colnames(G) <- paste0("g", 1:ncol(G))
+  
+  betas <- matrix(c(1, 0.5, 0, 0, 0.5, 0.1,
+                    0, -1, -2, 1, 0.5, 0.3,
+                    0, 0, 3, 1, 1, 1,
+                    0.5, 0.5, -1, 0.1, 0.2, 0.3,
+                    1, 0, 1, -0.5, 0.1, -0.5,
+                    -0.5, -0.5, -0.2, 0.2, 0.1, 0.3), 
+                  nrow = 6, byrow = TRUE)
+  
+  eta <- cbind(X, G) %*% betas
+  Rho_Y <- clusterGeneration::rcorrmatrix(d = 3, alphad = 1)
+  sds <- c(rep(0.5, 3))
+  Sigma_Y <- Rho_Y * (sds %*% t(sds))
+  
+  Y_con <- eta[, 1:3] + MASS::mvrnorm(n = n, mu = rep(0, 3), Sigma_Y)
+  Y_bin <- matrix(rbinom(n = n * 3, size = 1, prob = invLogit(eta[, 4:6])), nrow = n)
+  Y <- cbind(Y_con, Y_bin)
+  colnames(Y) <- paste0("y", 1:ncol(Y))
+  
+  return(as.data.frame(cbind(G, X, Y)))
+}
+
+set.seed(1004)
+pcsstools_example <- pcsstools_data(1000)
+save(pcsstools_example, file = "data/pcsstools_example.RData")
